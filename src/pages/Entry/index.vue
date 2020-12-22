7d67428420f8343415f2048abf56d861e95b6a3a
zhenglipeng <zhenglipeng@sensetime.com>
2020-12-22 14:48:53 +0800

    配置简化

--- src/pages/Entry/index.vue
+++ src/pages/Entry/index.vue
@@ -22,16 +22,49 @@ export default {
   methods: {
     prepareConfig(config) {
       return {
-        ...config,
-        components: config.components.map(this.prepareComponent)
+        components: this.prepareComponents(config.components),
+        form: this.prepareComponents(config.form),
+        reference: this.prepareComponents(config.reference),
       };
+    },
+    prepareComponents(components) {
+      if (!components) {
+        return components;
+      }
+      return components.map(this.prepareComponent);
     },
     prepareComponent(component) {
       if (typeof component === 'string') {
         const [type, title, tag, ...props] = component.split('|');
         return { type, title, tag, props };
       }
-      return component;
+      const args = component.type.split('|');
+      if (args.length > 1) {
+        const [type, title, tag, ...props] = args;
+        return {
+          title,
+          tag,
+          props,
+          ...component,
+          components: this.prepareComponents(component.components),
+          options: this.prepareOptions(component.options),
+          type
+        };
+      }
+      return {
+        ...component,
+        components: this.prepareComponents(component.components),
+        options: this.prepareOptions(component.options),
+      };
+    },
+    prepareOptions(options) {
+      if (typeof options === 'string') {
+        return options.split('|').map(option => {
+          const o = option.split(':');
+          return { key: o[0], value: o[1] || o[0] };
+        });
+      }
+      return options;
     }
   }
 };
