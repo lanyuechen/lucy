791e4e32ff322e7450a020b40af66d31467f2395
zhenglipeng <zhenglipeng@sensetime.com>
2020-12-22 11:53:15 +0800

    bugfix

--- src/components/Viewer/index.vue
+++ src/components/Viewer/index.vue
@@ -43,20 +43,30 @@ export default {
     };
   },
   methods: {
+    // 这么写是为了防止连续多次emit操作会覆盖之前的值
+    handleInput(key, val) {
+      this.value[key] = val;
+      this.$emit('input', {
+        ...this.value,
+      });
+    },
+
     // handleInput(key, val) {
     //   this.$emit('input', {
-    //     ...this.$props.value,
+    //     ...this.value,
     //     [key]: val
     //   });
     // },
-    handleInput(key, val) {
-      // 直接emit会导致连续多次emit后面的数据覆盖前面的数据，因此在这里收集所有变化再提交到父组件
-      this.formData = Object.assign({}, this.value, this.formData); // 防止使用v-model时污染formData变量
-      this.formData[key] = val;
-      this.$nextTick(() => {
-        this.$emit('input', this.formData);
-      });
-    }
+
+    // 这种方式也是不行的，切换组件时会把之前的属性带到新的组件，造成十分诡异的bug
+    // handleInput(key, val) {
+    //   // 直接emit会导致连续多次emit后面的数据覆盖前面的数据，因此在这里收集所有变化再提交到父组件
+    //   this.formData = Object.assign({}, this.value, this.formData); // 防止使用v-model时污染formData变量
+    //   this.formData[key] = val;
+    //   this.$nextTick(() => {
+    //     this.$emit('input', this.formData);
+    //   });
+    // }
   }
 };
 </script>
