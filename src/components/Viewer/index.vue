7c6269b5d647e03e43cd8f1f289983b3814b4b58
zhenglipeng <zhenglipeng@sensetime.com>
2021-01-19 19:09:54 +0800

    优化

--- src/components/Viewer/index.vue
+++ src/components/Viewer/index.vue
@@ -1,14 +1,25 @@
 <template>
-  <el-form v-bind="formConfig" :model="value">
-    <template v-for="c in dataSource">
+  <el-form v-bind="formConfig" :model="value" class="theme-mini">
+    <template v-for="(c, idx) in dataSource">
       <el-form-item
+        v-if="linkageShow(c.linkage, value)"
+        :class="{
+          'form-item-dynamic': c.type === 'dynamic',
+          'form-item-view': c.type === 'view',
+          'form-item-collapsed': collapsed.includes(idx)
+        }"
         :key="c.id"
         :prop="c.key"
         :label="c.title"
         :rules="c.rules | prepareRules"
       >
+        <template v-if="['dynamic', 'view'].includes(c.type)" slot="label">
+          <div class="collapsed-btn" @click="toggle(idx)">
+            {{ collapsed.includes(idx) ? '+' : '-'}}
+          </div>
+          {{ c.title }}
+        </template>
         <form-item
-          v-if="linkageShow(c.linkage, value)"
           :config="c | prepareConfig(source)"
           :value="value[c.key]"
           :disabled="linkageEnable(c.linkage, value)"
@@ -21,6 +32,7 @@
 
 <script>
 import { linkageShow, linkageEnable, prepareRules, prepareConfig } from '@/utils/util';
+import './theme.scss';
 
 export default {
   filters: {
@@ -52,10 +64,18 @@ export default {
   // ],
   data() {
     return {
-
+      collapsed: []
     };
   },
   methods: {
+    toggle(i) {
+      const idx = this.collapsed.indexOf(i);
+      if (idx > -1) {
+        this.collapsed.splice(idx, 1);
+      } else {
+        this.collapsed.push(i);
+      }
+    },
     handleInput(key, val) {
       this.value[key] = val;
       this.$emit('input', {
