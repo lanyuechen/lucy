86f31bceb5ccd70e8504e079f9dfed3dc7b947d9
zhenglipeng <zhenglipeng@sensetime.com>
2021-02-02 18:18:59 +0800

    上传组件更新

--- src/wardrobe/basic/Upload.vue
+++ src/wardrobe/basic/Upload.vue
@@ -1,10 +1,35 @@
 <template>
   <el-upload
     drag
-    action="https://jsonplaceholder.typicode.com/posts/"
-    multiple>
+    multiple
+    action=""
+    :before-upload="beforeUpload"
+    :before-remove="beforeRemove"
+    :file-list="value"
+  >
     <i class="el-icon-upload"></i>
     <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
     <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
   </el-upload>
 </template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    beforeUpload(file) {
+      this.$emit('input', [...this.value, file]);
+      return false;
+    },
+    beforeRemove(file, fileList) {
+      this.$emit('input', fileList.filter(d => d !== file));
+      return false;
+    }
+  }
+};
+</script>
