f14f1e4e611db578245224da64c6bd5f2e5eb078
zhenglipeng <zhenglipeng@sensetime.com>
2021-02-05 15:40:05 +0800

    提取vue,element-ui组件

--- src/main.js
+++ src/main.js
@@ -3,24 +3,24 @@
 import Vue from 'vue';
 import App from './App';
 import Element from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css'; // script导入
 
 import VueRouter from 'vue-router';
 
-import ace from 'ace-builds';
+// import ace from 'ace-builds';
 
 import { Splitpanes, Pane } from 'splitpanes';
 import 'splitpanes/dist/splitpanes.css';
 
 import Viewer from '@/components/Viewer';
-import Ace from '@/components/Ace';
+// import Ace from '@/components/Ace';
 
 import routes from './routes';
 
-Vue.use(ace);
+// Vue.use(ace);
 Vue.use(VueRouter);
 
-Vue.component('ace', Ace);
+// Vue.component('ace', Ace);
 Vue.component('splitpanes', Splitpanes);
 Vue.component('pane', Pane);
 Vue.component('viewer', Viewer);
