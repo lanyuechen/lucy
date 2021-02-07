7537072dd10fb455d156732c2cc45e564048499d
zhenglipeng <zhenglipeng@sensetime.com>
2021-02-07 12:41:09 +0800

    添加库打包,添加使用demo

---
+++ build/publish.js
@@ -0,0 +1,77 @@
+'use strict'
+require('./check-versions')()
+
+process.env.NODE_ENV = 'library'
+
+const ora = require('ora')
+const path = require('path')
+const utils = require('./utils')
+const chalk = require('chalk')
+const webpack = require('webpack')
+const config = require('../config')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const env = process.env.NODE_ENV === 'testing'
+  ? require('../config/test.env')
+  : require('../config/prod.env')
+
+const webpackConfig = merge(baseWebpackConfig, {
+  context: path.resolve(__dirname, '../'),
+  entry: './src/components/Viewer',
+  output:{
+    path: config.build.assetsRoot,
+    filename: utils.assetsPath('lucy.js'),
+    library: 'Lucy', // 在全局变量中增加一个library变量
+    libraryTarget: 'umd'
+  },
+  devtool: config.build.productionSourceMap ? config.build.devtool : false,
+  plugins: [
+    // http://vuejs.github.io/vue-loader/en/workflow/production.html
+    new webpack.DefinePlugin({
+      'process.env': env
+    }),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false
+        }
+      },
+      sourceMap: config.build.productionSourceMap,
+      parallel: true
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'demo.html',
+      template: 'demo.html',
+      inject: false,
+    }),
+  ]
+})
+
+const spinner = ora('building for library...')
+spinner.start()
+
+webpack(webpackConfig, (err, stats) => {
+  spinner.stop()
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+    chunks: false,
+    chunkModules: false
+  }) + '\n\n')
+
+  if (stats.hasErrors()) {
+    console.log(chalk.red('  Build failed with errors.\n'))
+    process.exit(1)
+  }
+
+  console.log(chalk.cyan('  Build complete.\n'))
+  console.log(chalk.yellow(
+    '  Tip: built files are meant to be served over an HTTP server.\n' +
+    '  Opening index.html over file:// won\'t work.\n'
+  ))
+})
